7742ccfb06258554dd6da64616f1c37833c90e17
nazar-yednak <93836409+nazar-yednak@users.noreply.github.com>
2022-07-25 00:59:10 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Profile } from "../Profile/Profile";
-import { Statistics } from "../Statistics/Statistics";
-import { FriendList } from "../FriendList/FriendList";
-import {TransactionHistory} from "../TransactionHistory/TransactionHistory"
+import { Profile } from "./components/Profile/Profile";
+import { Statistics } from "./components/Statistics/Statistics";
+import { FriendList } from "./components/FriendList/FriendList";
+import {TransactionHistory} from "./components/TransactionHistory/TransactionHistory"
 import user from '../user'
 import friends from '../friends'
 import data from '../data'
